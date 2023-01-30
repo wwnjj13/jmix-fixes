197d09ff38f61043a843ae41644449c5ccf43253
Gurylev Sergei Vitalevich <s.gurylev@haulmont.com>
2023-01-30 15:02:39 +0400

    [Grid Export Actions] Support for choosing the data range for export

--- jmix-ui/ui-export/src/main/java/io/jmix/uiexport/ExporterProperties.java
+++ jmix-ui/ui-export/src/main/java/io/jmix/uiexport/ExporterProperties.java
@@ -18,7 +18,6 @@ package io.jmix.uiexport;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "jmix.ui.exporter")
